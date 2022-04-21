2fb37307e625c0d4157992d14b6bdafec6df3763
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-04-21 17:33:14 +0200

    end

--- js/script.js
+++ js/script.js
@@ -1,29 +1,68 @@
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+const app = new Vue({
+    el: '#app',
+    data: {
+        slides: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ],
+        activeIndex: 0,
     },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    }
-];
+    methods: {
+        prevSlide() {
+            if (this.activeIndex === 0) {
+                this.activeIndex = 4;
+            } else {
+                this.activeIndex -= 1;
+            }
+        },
+        nextSlide() {
+            if (this.activeIndex === 4) {
+                this.activeIndex = 0;
+            } else {
+                this.activeIndex += 1;
+            }
+        },
+        focusImg(index) {
+            this.activeIndex = index;
 
-console.log(slides);
+        },
+        stopAutoScroll() {
+            clearInterval(interval);
+        },
+        autoScroll() {
+            interval = setInterval(() => {
+                this.nextSlide();
+            }, 3000);
+        }
+
+    },
+    mounted() {
+        this.autoScroll();
+
+    }
+})
+
+
